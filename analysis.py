949db28484556ecf364ca17bafd6dcab9a100968
magrathj <magrathj@tcd.ie>
2019-09-22 22:21:13 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -6,7 +6,7 @@ from pyspark.sql import SparkSession
 import requests, zipfile, io, os
 import pandas as pd 
 from pyspark import SparkContext, SparkConf
-
+from pyspark.sql.functions import avg
 
 #%% [markdown]
 #  Since we're using Spark locally we already have both a sparkcontext and a sparksession running. We can update some of the parameters, such our application's name. Let's just call it "Our first Python Spark SQL example"
@@ -39,5 +39,23 @@ df_policies = spark.read.format("csv").option("header","true").option("mode","DR
 df_policies.take(5) 
 
 
+
+#%%
+df_claims.describe().show()
+
+#%%
+df_policies.describe().show()
+
+
+#%% [markdown]
+#  claims analysis
+
+#%%
+df_claims.select('state').distinct().rdd.map(lambda r:r[0]).collect()
+
+
+#%%
+for col in df_claims.columns:
+    print(col, "\t", "with null values:", df_claims.filter(df_claims[col].isNull().count()))
 
 #%%
